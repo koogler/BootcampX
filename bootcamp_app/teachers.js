41653d76689fd97914790aa208d73c4928b1aae5
Desmond Parker <evan.parker95@gmail.com>
2022-01-06 00:33:27 +0000

    revising js queries to using parameterized queries instead

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,7 +7,10 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-const cohortName = process.argv[2]
+const cohortName = process.argv[2];
+// Store all potentially malicious values in an array.
+const values = [`%${cohortName}%`];
+
 
 pool.query(`
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
@@ -15,9 +18,9 @@ FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+WHERE cohorts.name LIKE $1
 ORDER BY teacher;
-`)
+`, values)
   .then(res => {
     res.rows.forEach(row => {
       console.log(`${row.cohort}: ${row.teacher}`);
